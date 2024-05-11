7069554cc19f519af16d0f3a5f9818413074ab69
kyh3423a <kyh3423a@gmail.com>
2024-05-11 18:02:55 +0900

    초기 커밋.

---
+++ Sku_Scaner/Form1.Designer.vb
@@ -0,0 +1,166 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기에서는 수정하지 마세요.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.파일ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.열기EzAdminToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.열기ShopeeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.열기Qoo10ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.Textbox1 = New System.Windows.Forms.TextBox()
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ToolStripProgressBar1 = New System.Windows.Forms.ToolStripProgressBar()
+        Me.MenuStrip1.SuspendLayout()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.파일ToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        '파일ToolStripMenuItem
+        '
+        Me.파일ToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.열기EzAdminToolStripMenuItem, Me.열기ShopeeToolStripMenuItem, Me.열기Qoo10ToolStripMenuItem})
+        Me.파일ToolStripMenuItem.Name = "파일ToolStripMenuItem"
+        Me.파일ToolStripMenuItem.Size = New System.Drawing.Size(43, 20)
+        Me.파일ToolStripMenuItem.Text = "파일"
+        '
+        '열기EzAdminToolStripMenuItem
+        '
+        Me.열기EzAdminToolStripMenuItem.Name = "열기EzAdminToolStripMenuItem"
+        Me.열기EzAdminToolStripMenuItem.Size = New System.Drawing.Size(154, 22)
+        Me.열기EzAdminToolStripMenuItem.Text = "열기(EzAdmin)"
+        '
+        '열기ShopeeToolStripMenuItem
+        '
+        Me.열기ShopeeToolStripMenuItem.Name = "열기ShopeeToolStripMenuItem"
+        Me.열기ShopeeToolStripMenuItem.Size = New System.Drawing.Size(154, 22)
+        Me.열기ShopeeToolStripMenuItem.Text = "열기(Shopee)"
+        '
+        '열기Qoo10ToolStripMenuItem
+        '
+        Me.열기Qoo10ToolStripMenuItem.Name = "열기Qoo10ToolStripMenuItem"
+        Me.열기Qoo10ToolStripMenuItem.Size = New System.Drawing.Size(154, 22)
+        Me.열기Qoo10ToolStripMenuItem.Text = "열기(Qoo10)"
+        '
+        'ListView1
+        '
+        Me.ListView1.Activation = System.Windows.Forms.ItemActivation.OneClick
+        Me.ListView1.CheckBoxes = True
+        Me.ListView1.GridLines = True
+        Me.ListView1.HideSelection = False
+        Me.ListView1.HoverSelection = True
+        Me.ListView1.Location = New System.Drawing.Point(12, 89)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(776, 349)
+        Me.ListView1.TabIndex = 1
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.Details
+        '
+        'Textbox1
+        '
+        Me.Textbox1.Location = New System.Drawing.Point(12, 27)
+        Me.Textbox1.Name = "Textbox1"
+        Me.Textbox1.Size = New System.Drawing.Size(629, 21)
+        Me.Textbox1.TabIndex = 2
+        Me.Textbox1.Text = "567742313250"
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon1.Text = "Isntree"
+        Me.NotifyIcon1.Visible = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1000
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1, Me.ToolStripProgressBar1})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 428)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(800, 22)
+        Me.StatusStrip1.SizingGrip = False
+        Me.StatusStrip1.TabIndex = 3
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(123, 17)
+        Me.ToolStripStatusLabel1.Text = "ToolStripStatusLabel1"
+        '
+        'ToolStripProgressBar1
+        '
+        Me.ToolStripProgressBar1.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right
+        Me.ToolStripProgressBar1.Name = "ToolStripProgressBar1"
+        Me.ToolStripProgressBar1.Size = New System.Drawing.Size(100, 16)
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.Textbox1)
+        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Sku_Scaner"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents 파일ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents 열기EzAdminToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents 열기ShopeeToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents 열기Qoo10ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ListView1 As ListView
+    Friend WithEvents Textbox1 As TextBox
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
+    Friend WithEvents ToolStripProgressBar1 As ToolStripProgressBar
+End Class
---
+++ Sku_Scaner/Form1.vb
@@ -0,0 +1,78 @@
+﻿Imports OfficeOpenXml
+Imports System.IO
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class Form1
+
+    Dim filePath As String = "D:\sku_Scaner\data2024.xlsx" ' 엑셀 파일 경로
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ExcelPackage.LicenseContext = LicenseContext.NonCommercial
+
+        ' 엑셀 파일 경로
+        Dim fileInfo As New FileInfo(filePath)
+
+        Using package As New ExcelPackage(fileInfo)
+            Dim worksheet As ExcelWorksheet = package.Workbook.Worksheets(0)
+
+            ' ListView 설정 초기화
+            ListView1.Columns.Clear()
+            ListView1.Items.Clear()
+
+            ' 직접 지정한 헤더를 배열로 정의합니다.
+            Dim customHeaders() As String = {"송장번호", "바코드", "연동코드", "상품명", "수량", "검수수량"}
+
+            ' ListView의 열을 설정합니다.
+            For Each header As String In customHeaders
+                ListView1.Columns.Add(header)
+            Next
+
+            ' 사용자가 입력한 조건을 텍스트 상자에서 읽어옴
+            Dim condition As String = Textbox1.Text
+
+            ' Dictionary를 사용하여 바코드를 키로, 해당 바코드의 총 수량과 기타 정보를 값으로 저장
+            Dim barcodeInfo As New Dictionary(Of String, (Integer, String, String))
+
+            For row As Integer = 2 To worksheet.Dimension.End.Row ' 1행은 헤더이므로 2행부터 시작
+                ' 사용자가 입력한 조건과 현재 행의 첫 번째 열 값이 일치하는지 확인
+                If worksheet.Cells(row, 2).Text = condition Then
+                    Dim barcode As String = worksheet.Cells(row, 3).Text ' 바코드 가져오기
+                    Dim productName As String = worksheet.Cells(row, 4).Text ' 상품명 가져오기
+                    Dim quantity As Integer = Integer.Parse(worksheet.Cells(row, 5).Text) ' 수량 가져오기
+                    Dim linkageCode As String = worksheet.Cells(row, 6).Text ' 연동코드 가져오기
+
+                    ' Dictionary에 해당 바코드가 이미 있는지 확인하고, 없으면 추가하고 있으면 수량을 더함
+                    If Not barcodeInfo.ContainsKey(barcode) Then
+                        barcodeInfo.Add(barcode, (quantity, productName, linkageCode))
+                    Else
+                        Dim currentInfo = barcodeInfo(barcode)
+                        barcodeInfo(barcode) = (currentInfo.Item1 + quantity, currentInfo.Item2, currentInfo.Item3)
+                    End If
+                End If
+            Next
+
+            ' ListView에 합쳐진 데이터를 표시
+            For Each kvp As KeyValuePair(Of String, (Integer, String, String)) In barcodeInfo
+                Dim newRow As New ListViewItem(condition) ' 조건 추가
+                newRow.SubItems.Add(kvp.Key) ' 바코드 추가
+                newRow.SubItems.Add(kvp.Value.Item3) ' 연동코드 추가
+                newRow.SubItems.Add(kvp.Value.Item2) ' 상품명 추가
+                newRow.SubItems.Add(kvp.Value.Item1.ToString()) ' 총 수량 추가
+                newRow.SubItems.Add(0) ' 연동코드 추가
+                ListView1.Items.Add(newRow)
+            Next
+
+
+        End Using
+
+        ' 모든 컬럼 너비 자동 조절
+        For Each column As ColumnHeader In ListView1.Columns
+            column.Width = -2
+        Next
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        ToolStripStatusLabel1.Text = DateTime.Now.ToString("HH:mm:ss")
+
+    End Sub
+End Class
